0699151800585be62fbcec1167c40ae61ba93ce3
divyakantvanajara <divyakant.1452@gmail.com>
2023-06-13 09:57:23 +0530

    12-6-2023

--- lesson4.js
+++ lesson4.js
@@ -1,4 +1,5 @@
 var converter =require("./MyModule2");
+var title =require("./MyModule3")
 var rupees =1000;
 var dollar = converter.toDollar(rupees);
 console.log(dollar);
@@ -6,3 +7,4 @@ var pound = converter.toPound(rupees);
 console.log(pound);
 var yen = converter.toYen(rupees);
 console.log(yen);
+console.log(title);
