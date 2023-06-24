1a8f946107e71cedb25fd653db5d30ea0d315983
divyakantvanajara <divyakant.1452@gmail.com>
2023-06-24 09:39:15 +0530

    express

--- express1.js
+++ express1.js
@@ -1,54 +1,22 @@
 var express = require("express");
-var fs =require("fs");
 var app = express();
 
-app.get('/home',function(request,response){
-    fs.readFile('home.html',function(error,FileContent){
-        response.send(FileContent.toString());
-    })
-   
+app.get("/",function(request,response){
+    response.send("this root/home page request");
 });
-app.get('/contactus',function(request,response){
-    fs.readFile('contactus.html',function(error,FileContent){
 
-        response.send(FileContent.toString());
-    })
+app.get("/contactus",function(request,response){
+    response.send("this is contact us page");
 });
-app.get('/aboutus',function(request,response){
-    fs.readFile('aboutus.html',function(error,FileContent){
 
-        response.send(FileContent.toString());
-    })
+app.get("/aboutus",function(request,response){
+    response.send("this is about us page");
 });
-app.get('/courses',function(request,response){
-    fs.readFile('courses.html',function(error,FileContent){
 
-        response.send(FileContent.toString());
-    })
+app.get("*",function(request,response){
+    response.send("page not found");
 });
-app.get('/coursdetail',function(request,response){
-    fs.readFile('coursdetail.html',function(error,FileContent){
 
-        response.send(FileContent.toString());
-    })
-});
-app.get('/instructors',function(request,response){
-    fs.readFile('instructors.html',function(error,FileContent){
-    
-        response.send(FileContent.toString());
-    })
-});
-app.get('/instructordetail',function(request,response){
-    fs.readFile('instructordetail.html',function(error,FileContent){
-
-        response.send(FileContent.toString());
-    })
-});    
-app.all('*',function(request,response){
-    fs.readFile('404error.html',function(error,FileContent){
-
-        response.send(FileContent.toString());
-    })
-})
-app.listen(5000);
-console.log('ready to accept request');
+var portno = 5000;
+app.listen(portno);
+console.log('ready to accept request....');
